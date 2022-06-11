72cd15ae11309f1840f949b7875076e0b17d7e77
rohangawhade <gawhaderohan@gmail.com>
2022-06-11 18:31:00 +0530

    added scroll bar

--- frontend/src/Components/DisplayInfo.js
+++ frontend/src/Components/DisplayInfo.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Scrollbars } from 'react-custom-scrollbars';
+
 
 function DisplayInfo({ data }) {
   return (
@@ -7,13 +9,15 @@ function DisplayInfo({ data }) {
         <div id='poster'>
           <img src={data["Poster"]} alt="Movie Poster" />
         </div>
-        <div id='info'>
-          {
-            (data && Object.keys(data).map((k, i) => {
-              return <p key={i}><strong>{k}</strong>: {data[k]}</p>
-            }))
-          }
-        </div>
+        <Scrollbars style={{ height: "inherit" }}>
+          <div id='info'>
+            {
+              (data && Object.keys(data).map((k, i) => {
+                return <p key={i}><strong>{k}</strong>: {data[k]}</p>
+              }))
+            }
+          </div>
+        </Scrollbars>
       </div>
     </>
   )
